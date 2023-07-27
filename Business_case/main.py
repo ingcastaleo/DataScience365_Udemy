ed39aa916953a1254635a637eda5de14dc5467d3
ingcastaleo <72327848+ingcastaleo@users.noreply.github.com>
2023-07-26 23:16:07 -0500

    bc

--- Business_case/main.py
+++ Business_case/main.py
@@ -1,6 +1,6 @@
 import numpy as np
 from sklearn import preprocessing # To standardize the inputs scikit-learn
 
-cvs_data_url = "https://raw.githubusercontent.com/ingcastaleo/DataScience365_Udemy/main/Business_case/Audiobooks_data.csv"
+#cvs_data_url = "https://raw.githubusercontent.com/ingcastaleo/DataScience365_Udemy/main/Business_case/Audiobooks_data.csv"
 cvs_data_url = "Business_case\src\Audiobooks_data.csv"
 raw_csv_data = np.loadtxt(cvs_data_url,delimiter=',')
